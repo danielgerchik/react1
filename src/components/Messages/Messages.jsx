00ffad8aaee8e24a631ea60a72aac07683b985d1
Daniel <danielgerchik1@gmail.com>
2022-02-14 12:36:58 +0300

    messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -2,8 +2,19 @@ import s from './Messages.module.css';
 
 const Messages = props => {
     return (
-        <div>
-            Messages
+        <div className={s.messages}>
+           <div className={s.interlocutors}>
+               <div className={s.interlocutor}>Ваня</div>
+               <div className={s.interlocutor}>Петя</div>
+               <div className={s.interlocutor}>Маша</div>
+               <div className={s.interlocutor}>Даня</div>
+           </div>
+            <div className={s.dialogs}>
+                <div className={s.dialog}>Привет</div>
+                <div className={s.dialog}>Как дела ?</div>
+                <div className={s.dialog}>Че делаешь ?</div>
+                <div className={s.dialog}>Я знаю это</div>
+            </div>
         </div>
     )
 }
