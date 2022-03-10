48ad282b285fc07c7dd346396f5e7e06bb94e7bf
Daniel <danielgerchik1@gmail.com>
2022-03-10 21:11:10 +0300

    local state

--- src/components/Profile/Info/Info.jsx
+++ src/components/Profile/Info/Info.jsx
@@ -1,23 +1,56 @@
 import s from "./Info.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import profileIMG from "../../../img/25.jpg"
+import React from "react";
 
+class Info extends React.Component {
 
-const Info = (props) => {
-if(!props.userProfile) {
-    return <Preloader/>
-}
-    return (
-        <div className={s.about}>
-            <div className={s.img}>
-                <img src={props.userProfile.photos.large || 'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg'} alt=""/>
-            </div>
-            <div className={s.secription}>
-                <div className={s.name}>{props.userProfile.fullName}</div>
-                <div className={s.aboutMe}>{props.userProfile.aboutMe}
+    state = {
+        editMode: false,
+    }
+    enableEditMode = () => {
+        this.setState(
+            {
+                editMode: true,
+            }
+        )
+    }
+    stopEditMode = () => {
+        this.setState(
+            {
+                editMode: false,
+            }
+        )
+    }
+
+    render() {
+        if (!this.props.userProfile) {
+            return <Preloader/>
+        }
+        return (
+            <div className={s.about}>
+                <div className={s.img}>
+                    <img
+                        src={this.props.userProfile.photos.large || 'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg'}
+                        alt=""/>
+                </div>
+                <div className={s.secription}>
+                    <div className={s.status}>
+                        {
+                            this.state.editMode
+                                ? <input autoFocus={true} onBlur={this.stopEditMode} value={this.props.status}/>
+                                : <div onClick={this.enableEditMode}>{this.props.status}</div>
+                        }
+                    </div>
+
+                    <div className={s.name}>{this.props.userProfile.fullName}</div>
+                    <div className={s.aboutMe}>{this.props.userProfile.aboutMe}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
+
 export default Info;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import PostsContainer from "./Posts/PostsContainer";
 const Profile = props => {
     return (
         <div className={s.profile}>
-            <Info userProfile={props.userProfile}/>
+            <Info status= 'this is status... blabla' userProfile={props.userProfile}/>
             <PostsContainer/>
         </div>
     )
